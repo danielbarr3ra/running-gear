de8cee4773ff1adb7e765fa52e00f5eded99b05b
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-05-30 19:58:56 -0500

    added a NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
-    <div className="App">
+    <><NavBar /><div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div></>
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div id="navbar">
+            <div id="brand">Gea Running</div>
+            <ul>
+                <li><a href="http://github.com">Shoes</a></li>
+                <li><a href="http://github.com">Suplements</a></li>
+                <li><a href="http://github.com">Trail</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
