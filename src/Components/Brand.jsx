6d7fda5c1c209469b12b3650f57fff562420b9ff
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-06-15 16:41:30 -0500

    link in brand

--- src/Components/Brand.jsx
+++ src/Components/Brand.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { ChevronDoubleRightIcon } from "@heroicons/react/solid"
+import { Link } from "react-router-dom";
 
 
 
 const Brand = () => {
     return (
-        <>
+        <Link to={"/"}>
             <ChevronDoubleRightIcon className="h-10 w-10 text-black-100" />
             <p className="font-mono text-2xl italic font-black ">RunOn</p>
-        </>
+        </Link>
+
 
     )
 }
