cddeddfadf4fbb2c6a2ef6e303216b0087ea3e05
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-06-27 19:59:34 -0500

    link back to catalog

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import { CartContext } from "./CartContext";
 import CartListItem from "./CartListItem";
 import ItemListContainer from "./ItemListContainer";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     const { cart, isInCart, addItem, deleteItem, emptyCart, getItemQty, getItemPrice, test, cartSize } = useContext(CartContext)
@@ -33,15 +34,12 @@ const Cart = () => {
 
 
 
-                        <a href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
+                        <Link to={"/"} className="flex font-semibold text-indigo-600 text-sm mt-10">
 
                             <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
                             Continue Shopping
-                        </a>
+                        </Link>
                     </div>
-
-
-
                 </div>
             </div>
         </>
