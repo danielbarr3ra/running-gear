76215e073a95d8cca962375a97bdda73fc2f4a85
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-06-27 20:14:22 -0500

    shows message if cart is empty

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -7,10 +7,23 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
     const { cart, isInCart, addItem, deleteItem, emptyCart, getItemQty, getItemPrice, test, cartSize } = useContext(CartContext)
+
+    if (cart.length == 0) {
+        return (<><div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong className="font-bold">Hold on there buckaroo!</strong>
+            <span className="block sm:inline">Seems that you need to choses some shoes before checking out</span>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+            </span>
+            <br></br>
+        </div><Link to={"/"}>
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Lets go back shopping
+                </button>
+            </Link></>)
+    }
     return (
-        <><div>
-            {JSON.stringify(cart)}
-        </div>
+        <>
             <div className="container mx-auto mt-10">
                 <div className="flex shadow-md my-10">
                     <div className="w-3/4 bg-white px-10 py-10">
