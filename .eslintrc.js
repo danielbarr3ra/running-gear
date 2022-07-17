82f9355db3f46910b8d7acfaacc0ab953698e386
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-16 21:09:01 -0500

    used node instead of amd eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     "env": {
-        "amd": true
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
