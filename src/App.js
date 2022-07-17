237c7e9505b8d5edc4501c6e61c3d0c83f76f24d
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-16 20:46:38 -0500

    cleaned app imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-/* eslint-disable no-unused-vars */
 import './App.css';
 import NavBar from './Components/NavBar'
 import ItemListContainer from './Components/ItemListContainer';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ItemDetailContainer from './Components/ItemDetailContianer';
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
-import Home from './Components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Cart from './Components/Cart';
 import MyProvider from './Components/CartContext';
 import Test from './Components/Test';
-
-
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 
 function App() {
   const firebaseConfig = {
@@ -24,7 +20,7 @@ function App() {
     appId: "1:708748387871:web:b3790e7716f04d1aef972a"
   };
 
-  const app = initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
 
   return (
     <>
