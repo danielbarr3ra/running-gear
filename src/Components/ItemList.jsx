7d34bdc809da4bd410d2356ebe93f552ec248598
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-17 17:51:57 -0500

    cleanUp ItemList

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -1,12 +1,9 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-undef */
 import React from "react";
 import PropTypes from 'prop-types';
 import Item from './Item'
-//import ItemCount from "./ItemCount";
 
 
-const ItemList = ({ catalog, clickDetails }) => {
+const ItemList = ({ catalog }) => {
     return (
         <>
             <div className="wrapper max-w-sm flex justify-between text-black 800 rounded-lg bg-gray-100 shadow-lg" >
@@ -16,7 +13,7 @@ const ItemList = ({ catalog, clickDetails }) => {
                         return (
                             <>
                                 <Item key={id} id={id} title={title} model={model} price={price} stock={stock} imageUrl={imageUrl}
-                                    clickDetails={clickDetails} />
+                                />
                             </>
                         )
                     })
