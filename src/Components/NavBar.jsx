75b2d28d58683105f9fdf648817ba3e6d750fa4f
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-18 00:40:01 -0500

    NavBar responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,10 +4,17 @@ import CartWidget from "./CartWidget";
 import SearchBar from "./SearchBar";
 import AccountLogIn from "./AccountLogIn";
 import { Link } from "react-router-dom";
+import { ChevronDownIcon } from "@heroicons/react/solid"
+
+
 const NavBar = () => {
+    const displayNav = () => {
+        const menu = document.querySelector(".mobile-menu");
+        menu.classList.toggle("hidden")
+    }
     return (
         <>
-            <nav className="flex items-center flex-wrap p-6 bg-gray-100 bg-repeat w-full h-full heropattern-topography-red-400 justify-between">
+            <nav className="flex items-center flex-wrap p-6 bg-gray-100 bg-repeat w-full h-full heropattern-topography-red-400 justify-between hidden md:flex items-center space-x-1">
                 <div className="flex items-center ">
                     <Brand />
                 </div>
@@ -38,9 +45,50 @@ const NavBar = () => {
                     <AccountLogIn />
                 </div>
                 <div className="Cart flex">
-                    <CartWidget itemCount={2} />
+                    <CartWidget itemCount={0} />
                 </div>
             </nav>
+            {/* this is the hidden menu button for the mobile  */}
+            <div className=" p-6 bg-gray-100 bg-repeat w-full h-full heropattern-topography-red-400 md:hidden ">
+                <div className="flex items-cente justify-between">
+                    <div className="flex items-center ">
+                        <Brand />
+                    </div>
+                    <button className="outline-none mobile-menu-button" onClick={() => {
+                        displayNav()
+                    }}>
+                        <ChevronDownIcon className="h-10 w-10 text-black-100" />
+                    </button>
+                </div>
+                {/* hiddent mmenu */}
+                <div className="hidden mobile-menu flex-col">
+                    <ul className="flex-col items-stretch hover:items-center">
+                        <Link to={"/category/new"}>
+                            <li className="navTab">
+                                New
+                            </li>
+                        </Link>
+                        <Link to={"/category/road"}>
+                            <li className="navTab ">
+                                Road
+                            </li>
+                        </Link>
+                        <Link to={"/category/trail"}>
+                            <li className="navTab ">
+                                Trail
+                            </li>
+                        </Link>
+                        <Link to={"/category/apperal"}>
+                            <li className="navTab ">
+                                Apperal
+                            </li>
+                        </Link>
+                        <li className="Cart flex">
+                            <CartWidget itemCount={0} />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
