1b72a42575e78811e3b310df041c1d863f46f23a
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-18 00:02:29 -0500

    cleaning navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,42 +7,38 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <>
-            <nav className="flex items-center justify-between flex-wrap p-6 bg-gray-100 bg-repeat w-full h-full heropattern-topography-red-400">
-                <div className="flex items-center w-1/12">
+            <nav className="flex items-center flex-wrap p-6 bg-gray-100 bg-repeat w-full h-full heropattern-topography-red-400 justify-between">
+                <div className="flex items-center ">
                     <Brand />
                 </div>
-                <nav className="flex items-center justify-around w-4/12 font-semibold text-l">
-                    <Link to={"/category/new"}>
-                        <div className="navTab">
-                            New
-                        </div>
-                    </Link>
-                    <Link to={"/category/road"}>
-                        <div className="navTab ">
-                            Road
-                        </div>
-                    </Link>
-                    <Link to={"/category/trail"}>
-                        <div className="navTab ">
-                            Trail
-                        </div>
-                    </Link>
-                    <Link to={"/category/apperal"}>
-                        <div className="navTab ">
-                            Apperal
-                        </div>
-                    </Link>
-                </nav>
-                <div className="sessionNavigation flex justify-around w-3/12">
-                    <div className="searchBar">
-                        <SearchBar />
+                <Link to={"/category/new"}>
+                    <div className="navTab">
+                        New
                     </div>
-                    <div className="accountLogIn">
-                        <AccountLogIn />
+                </Link>
+                <Link to={"/category/road"}>
+                    <div className="navTab ">
+                        Road
                     </div>
-                    <div className="Cart flex">
-                        <CartWidget itemCount={2} />
+                </Link>
+                <Link to={"/category/trail"}>
+                    <div className="navTab ">
+                        Trail
                     </div>
+                </Link>
+                <Link to={"/category/apperal"}>
+                    <div className="navTab ">
+                        Apperal
+                    </div>
+                </Link>
+                <div className="searchBar">
+                    <SearchBar />
+                </div>
+                <div className="accountLogIn">
+                    <AccountLogIn />
+                </div>
+                <div className="Cart flex">
+                    <CartWidget itemCount={2} />
                 </div>
             </nav>
         </>
