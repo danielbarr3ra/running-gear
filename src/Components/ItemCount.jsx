d44334eb476f1ad9d9a99ad22b1f84b084f65755
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-18 16:28:38 -0500

    styled counter

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -19,18 +19,20 @@ const ItemCount = ({ stock, initial, addOn }) => {
         }
     }
     return (
-        <><div className="itemCount m-3 flex s">
-            <button className="addItem">
-                <PlusCircleIcon onClick={addingItem} className="h-8 w-8 text-black-900" />
-            </button>
-            <div className="item count text-xl">
-                {cartCount}
+        <>
+            <div className="flex justify-around">
+                <button className="addItem">
+                    <PlusCircleIcon onClick={addingItem} className="h-8 w-8 text-black-900" />
+                </button>
+                <div className="text-xl">
+                    {cartCount}
+                </div>
+                <button className="removeItem">
+                    <MinusCircleIcon onClick={removingItem} className="h-8 w-8 text-black-900" />
+                </button>
             </div>
-            <button className="removeItem">
-                <MinusCircleIcon onClick={removingItem} className="h-8 w-8 text-black-900" />
-            </button>
-        </div><div>
-                <button type="button" className="px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-black-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" onClick={() => { addOn(cartCount) }}>Add To Cart</button>
+            <div>
+                <button type="button" className="text-white bg-gray-700 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center" onClick={() => { addOn(cartCount) }}>Add To Cart</button>
             </div></>
 
     )
