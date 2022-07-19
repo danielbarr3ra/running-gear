474e8c673641901fe9aaea136088852db80b262e
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-18 22:47:28 -0500

    clear up cart after buying

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -9,11 +9,12 @@ import SubmitForm from "./SubmitForm";
 const Cart = () => {
     // for the modal pop up of the form
     const [formIsOpen, setFormOpen] = useState(false)
+    const [purchased, setPurchase] = useState(false)
     const openForm = () => { setFormOpen(true) }
     const closeForm = () => { setFormOpen(false) }
 
 
-    const { cart, cartSize, cartPrice } = useContext(CartContext)
+    const { cart, cartSize, cartPrice, emptyCart } = useContext(CartContext)
     const submitOrder = (name, email, address, payments) => {
         const order = {
             buyer: {
@@ -29,23 +30,42 @@ const Cart = () => {
         }
         const db = getFirestore();
         const orders = collection(db, 'orders')
-        addDoc(orders, order).then(({ id }) => alert(id))
+        addDoc(orders, order).then(({ id }) => alert(`order: ${id} has been submitted`))
+        emptyCart()
+        setPurchase(true)
     }
     if (cart.length == 0) {
-        return (<>
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <strong className="font-bold">Hold on there buckaroo!</strong>
-                <span className="block sm:inline">Seems that you need to choses some shoes before checking out</span>
-                <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
-                </span>
-                <br></br>
-            </div>
-            <Link to={"/"}>
-                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                    Lets go back shopping
-                </button>
-            </Link></>)
+        if (purchased) {
+            return (<>
+                <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong className="font-bold">Thank you for buying with use</strong>
+                    <span className="block sm:inline">lets go browswe more items</span>
+                    <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                    </span>
+                    <br></br>
+                </div>
+                <Link to={"/"}>
+                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                        Lets go back shopping
+                    </button>
+                </Link></>)
+        } else {
+            return (<>
+                <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong className="font-bold">Hold on there buckaroo!</strong>
+                    <span className="block sm:inline">Seems that you need to choses some shoes before checking out</span>
+                    <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                    </span>
+                    <br></br>
+                </div>
+                <Link to={"/"}>
+                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                        Lets go back shopping
+                    </button>
+                </Link></>)
+        }
     }
     return (
         <>
