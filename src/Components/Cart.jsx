dc5c5336d74644b7cda4c95f2b9f71aad61f30e4
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-18 21:10:25 -0500

    some styling, testing submit form

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,14 +3,10 @@ import { CartContext } from "./CartContext";
 import CartListItem from "./CartListItem";
 import { Link } from "react-router-dom";
 import { collection, getFirestore, addDoc } from "firebase/firestore"
-
+import SubmitForm from "./SubmitForm";
 
 const Cart = () => {
     const { cart, cartSize, cartPrice } = useContext(CartContext)
-    // const displayMobileCart = () => {
-    //     const mobileCart = document.querySelector(".mobile-cart");
-    //     menu.classList.toggle("hidden")
-    // }
     const submitOrder = () => {
         const order = {
             buyer: {
@@ -20,21 +16,24 @@ const Cart = () => {
             },
             items: {
                 ...cart
-            }
+            },
+            total: cartPrice
         }
         const db = getFirestore();
         const orders = collection(db, 'orders')
         addDoc(orders, order).then(({ id }) => alert(id))
     }
     if (cart.length == 0) {
-        return (<><div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <strong className="font-bold">Hold on there buckaroo!</strong>
-            <span className="block sm:inline">Seems that you need to choses some shoes before checking out</span>
-            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
-            </span>
-            <br></br>
-        </div><Link to={"/"}>
+        return (<>
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong className="font-bold">Hold on there buckaroo!</strong>
+                <span className="block sm:inline">Seems that you need to choses some shoes before checking out</span>
+                <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                </span>
+                <br></br>
+            </div>
+            <Link to={"/"}>
                 <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                     Lets go back shopping
                 </button>
@@ -42,6 +41,7 @@ const Cart = () => {
     }
     return (
         <>
+            <SubmitForm />
             <div className="container mx-auto mt-10">
                 <div className="flex flex-col md:flex-row shadow-md my-10">
                     <div className="w-full md:w-8/12 bg-white px-10 py-10">
@@ -93,7 +93,9 @@ const Cart = () => {
                                 <span>Total cost</span>
                                 <span>{cartPrice - 10}</span>
                             </div>
-                            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full" onClick={submitOrder}>Checkout</button>
+                            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full" onClick={() => {
+                                submitOrder()
+                            }}>Checkout</button>
                         </div>
                     </div>
                 </div>
--- src/Components/CartListItem.jsx
+++ src/Components/CartListItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useContext } from "react";
 import PropTypes from 'prop-types'
 import { CartContext } from "./CartContext";
@@ -11,32 +12,30 @@ const CartListItem = ({ location }) => {
         <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
             <div className="flex w-2/5">
                 <div className="w-20">
-                    <img className="h-24" src={require(`../${imageUrl}`)} alt="" ></img>
+                    <img className="h-24" src={require(`../${imageUrl}`)} alt=""></img>
                 </div>
                 <div className="flex flex-col justify-between ml-4 flex-grow">
                     <span className="font-bold text-sm">{model}</span>
-                    <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs" onClick={
-                        () => {
-                            deleteItem(id)
-                        }
-                    }>Remove</a>
+                    <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs" onClick={() => {
+                        deleteItem(id);
+                    }}>Remove</a>
                 </div>
             </div>
             <div className="flex justify-center w-1/5">
                 <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512" onClick={() => {
-                    addItem({ ...shoe }, -1)
+                    addItem({ ...shoe }, -1);
                     if (shoe.quantity == 0) {
-                        deleteItem(id)
+                        deleteItem(id);
                     }
                 }}><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                 </svg>
 
-                <input className="mx-2 border text-center w-8" type="text" value={quantity} />
+                <input className="mx-2 border text-center w-12" type="text" value={quantity} />
 
                 <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512" onClick={() => {
-                    addItem({ ...shoe }, +1)
+                    addItem({ ...shoe }, +1);
                     if (shoe.quantity == 0) {
-                        deleteItem(id)
+                        deleteItem(id);
                     }
                 }}>
                     <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
